be70e4a4cb591b89120be328db093a937b037c5b
Jonathan Adams <jd.adams16@gmail.com>
2021-01-20 23:34:29 -0600

    Add user icon.

--- client/app.ts
+++ client/app.ts
@@ -18,6 +18,17 @@ import 'bootstrap-vue/dist/bootstrap-vue.css';
 Vue.use(BootstrapVue);
 
 // ---------------------------------------------------------------------------------------------------------------------
+// Font Awesome
+// ---------------------------------------------------------------------------------------------------------------------
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(far);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+// ---------------------------------------------------------------------------------------------------------------------
 // Vue Router
 // ---------------------------------------------------------------------------------------------------------------------
 
--- client/components/header.vue
+++ client/components/header.vue
@@ -5,18 +5,19 @@
       <b-navbar-nav class="ml-auto">
         <b-nav-form>
           <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <font-awesome-icon :icon="['far', 'user-circle']" size="lg" class="text-white"></font-awesome-icon>
         </b-nav-form>
       </b-navbar-nav>
     </b-navbar>
   </div>
 </template>
 
+<style scoped>
+
+</style>
+
 <script>
 export default {
 name: "header"
 }
 </script>
-
-<style scoped>
-
-</style>
