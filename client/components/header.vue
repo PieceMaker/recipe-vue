62759ec4613521e5a3fdd5023c40420e22be690d
Jonathan Adams <jd.adams16@gmail.com>
2021-01-20 21:51:26 -0600

    Add search bar to header.

--- client/components/header.vue
+++ client/components/header.vue
@@ -2,6 +2,11 @@
   <div class="site-header">
     <b-navbar type="dark" variant="dark">
       <b-navbar-brand>Recipes</b-navbar-brand>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+        </b-nav-form>
+      </b-navbar-nav>
     </b-navbar>
   </div>
 </template>
