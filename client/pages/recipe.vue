0abe8af85b100f110aebbf7d2f4d904f2bc81017
Jonathan Adams <jd.adams16@gmail.com>
2021-04-04 22:59:06 -0500

    Added title and publishing info.

--- client/pages/recipe.vue
+++ client/pages/recipe.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="recipe">
-<!--    <div class="img-placeholder text-center" align-v="center">-->
-<!--      Image-->
-<!--    </div>-->
     <b-row class="img-placeholder mx-auto" align-v="center" align-h="center">
       Image Placeholder
     </b-row>
+    <div class="info text-center">
+      <h1 style="display: inline;">Title</h1>
+      <h4 style="display: inline;">- Author</h4>
+      <p class="text-muted">Published/Updated: 10/31/20XX</p>
+    </div>
   </div>
 </template>
 
@@ -15,6 +17,10 @@
     width: 600px;
     background-color: #808080;
     color: #DCDCDC;
+  }
+
+  .info {
+    padding-top: 25px;
   }
 </style>
 
